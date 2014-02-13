f2333964f065ec5fd6c8906ad2fe728349285f7d
Jungkwan Kim <donationsurvey@gmail.com>
2014-02-14 03:35:40 +0900

    Update class-wc-gateway-kcp-card.php
    https://github.com/hongsw/ajin-kcp-payment-woocommerce/issues/11 에 관련한 오류 수정.

--- woocommerce-KCP-plugin/gateway/kcp/class-wc-gateway-kcp-card.php
+++ woocommerce-KCP-plugin/gateway/kcp/class-wc-gateway-kcp-card.php
@@ -778,7 +778,8 @@ class WC_Gateway_KCP_card extends WC_Gateway_KCP {
 		
 		return array(
 			'result' 	=> 'success',
-			'redirect'	=> add_query_arg('order', $order->id, add_query_arg('key', $order->order_key, get_permalink(get_option('woocommerce_pay_page_id'))))
+			'redirect'	=> add_query_arg('order', $order->id, add_query_arg('key', $order->order_key, get_permalink(woocommerce_get_page_id('pay')
+)))
     		);
 	}
 	
